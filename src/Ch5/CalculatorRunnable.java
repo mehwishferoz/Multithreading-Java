32eb96683abf3f4eec5016d2ac92edcfb7b98abf
mehwxsh <wish.meh03@gmail.com>
2024-08-20 15:09:55 +0530

    Ch 5

--- src/Ch5/CalculatorRunnable.java
+++ src/Ch5/CalculatorRunnable.java
@@ -1,4 +1,18 @@
 package Ch5;
 
-public class CalculatorRunnable {
+public class CalculatorRunnable implements Runnable{
+    long value;
+    public CalculatorRunnable(long value) {
+        this.value = value;
+    }
+    @Override
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        long sum = 0;
+        for (long i = 0; i < value; i++) {
+            sum+=i;
+        }
+        long endTime = System.currentTimeMillis() - startTime;
+        System.out.println("Elapsed time: " + endTime + " for thread: " + Thread.currentThread().getName());
+    }
 }
